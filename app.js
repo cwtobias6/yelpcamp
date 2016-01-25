61cc2761560f508777856b609db2857637cb9f34
Christian Tobias <cwtobias6@gmail.com>
2016-01-25 13:48:53 -0600

    added: mongoDB,Mongoose, and CRUD

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+
+//schema setup
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+// 	{
+// 		name: "Granite Hill", 
+// 		image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg",
+// 	}, function(err, campground) {
+// 		if(err) {
+// 			console.log(err);
+// 		} else {
+// 			console.log("A New Campground has been created!!");
+// 			console.log(campground);
+// 		}
+// 	});
 
 var campgrounds = [
 		{name: "Salmon Creek", image: "https://farm4.staticflickr.com/3514/3844623716_427ed81275.jpg"},
@@ -20,8 +46,14 @@ app.get('/', function (req, res) {
 });
 
 app.get("/campgrounds", function(req,res) {
-
-	res.render("campgrounds", {campgrounds:campgrounds});
+	Campground.find({}, function(err, allCampgrounds) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds", {campgrounds:allCampgrounds});
+			
+		}
+	})
 });
 
 app.post("/campgrounds", function(req,res) {
@@ -29,9 +61,14 @@ app.post("/campgrounds", function(req,res) {
 	var image = req.body.image;
 
 	var newCampground = {name:name, image:image};
-	campgrounds.push(newCampground);
 
-	res.redirect("/campgrounds");
+	Campground.create(newCampground, function(err,newlyCreated) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");	
+		}
+	})
 
 
 });
---
+++ views/dbpractice.js
@@ -0,0 +1,2 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');

8cfa6bba7e2db6a93220befde89b8484c3c008d4
Christian Tobias <cwtobias6@gmail.com>
2016-01-28 10:09:41 -0600

    added add a comment functionality

--- app.js
+++ app.js
@@ -68,8 +68,33 @@ app.get("/campgrounds/:id", function(req, res) {
 
 // Comments Routes
 app.get("/campgrounds/:id/comments/new", function(req,res) {
-	res.render("comments/new");
-})
+	Campground.findById(req.params.id, function(err,campground) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("comments/new", {campground: campground});	
+		}
+	})
+});
+
+app.post("/campgrounds/:id/comments", function(req,res) {
+	Campground.findById(req.params.id, function(err, campground) {
+		if(err) {
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			Comment.create(req.body.comment, function(err, comment) {
+				if(err) {
+					console.log(err);
+				} else {
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			})
+		}
+	})
+});
 
 app.listen(3000, function () {
   console.log('The Server has Started!!');

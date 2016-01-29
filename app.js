6fd0b900e5bf5e9d3c4965da7f2b2f83a140e6d5
Christian Tobias <cwtobias6@gmail.com>
2016-01-29 13:25:53 -0600

    added login and logout functionality, and added current user to all pages

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req,res,next) {
+	res.locals.currentUser = req.user;
+	next();
+})
 
 app.get('/', function (req, res) {
   res.render('landing');
@@ -82,7 +86,7 @@ app.get("/campgrounds/:id", function(req, res) {
 });
 
 // Comments Routes
-app.get("/campgrounds/:id/comments/new", function(req,res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req,res) {
 	Campground.findById(req.params.id, function(err,campground) {
 		if(err) {
 			console.log(err);
@@ -92,7 +96,7 @@ app.get("/campgrounds/:id/comments/new", function(req,res) {
 	})
 });
 
-app.post("/campgrounds/:id/comments", function(req,res) {
+app.post("/campgrounds/:id/comments",isLoggedIn, function(req,res) {
 	Campground.findById(req.params.id, function(err, campground) {
 		if(err) {
 			console.log(err);
@@ -143,7 +147,17 @@ app.post("/login", passport.authenticate("local",
 	
 });
 
+app.get("/logout", function(req,res) {
+	req.logout();
+	res.redirect("/campgrounds");
+});
 
+function isLoggedIn(req,res,next) {
+	if(req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect("/login");
+}
 
 
 app.listen(3000, function () {

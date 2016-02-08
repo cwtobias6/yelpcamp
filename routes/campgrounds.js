08ac20c99e77adb5446d7386eb3bda5553b4e177
Christian Tobias <cwtobias6@gmail.com>
2016-02-08 14:02:04 -0600

    added edit functionality

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -67,6 +67,16 @@ router.get("/campgrounds/:id/edit", function(req,res){
 
 });
 
+router.put("/campgrounds/:id", function(req,res){
+	Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+		if(err) {
+			res.redirect("/campgrounds");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
 function isLoggedIn(req,res,next) {
 	if(req.isAuthenticated()) {
 		return next();

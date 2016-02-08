be3e5493ada20f25f2d10c45105c17689d27f48a
Christian Tobias <cwtobias6@gmail.com>
2016-02-08 11:12:14 -0600

    added submitted by for campsites

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -17,7 +17,11 @@ router.post("/campgrounds", isLoggedIn, function(req,res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var desc = req.body.description;
-	var newCampground = {name:name, image:image, description:desc}
+	var author = {
+		id: req.user._id,
+		username: req.user.username
+	}
+	var newCampground = {name:name, image:image, description:desc, author: author}
 
 
 	Campground.create(newCampground, function(err,newlyCreated) {
